a20edeae8882fa4fbe9b8d5f6fc3c44ff620c4ac
Binh Lam <cindybinh444@gmail.com>
2018-08-24 15:06:07 -0400

    remove all console.log

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,19 +17,16 @@ $(document).ready(function() {
   //Buttons click for each crystal
 
   $("#crystal-red-img").on("click", function() {
-    console.log((totalScoreNum += crystalRedNum));
-    console.log(totalScoreNum);
+    totalScoreNum += crystalRedNum;
     $("#total-score-num").text(totalScoreNum);
 
     if (totalScoreNum === randomNum) {
       winNum++;
-      console.log(winNum);
       $("#win-num").text(winNum);
       $("#win-lose-message").text("You Win!!!");
       startGame();
     } else if (totalScoreNum > randomNum) {
       loseNum++;
-      console.log(loseNum);
       $("#lose-num").text(loseNum);
       $("#win-lose-message").text("You Lose!!!");
       startGame();
@@ -37,19 +34,16 @@ $(document).ready(function() {
   });
 
   $("#crystal-blue-img").on("click", function() {
-    console.log((totalScoreNum += crystalBlueNum));
-    console.log(totalScoreNum);
+    totalScoreNum += crystalBlueNum;
     $("#total-score-num").text(totalScoreNum);
 
     if (totalScoreNum === randomNum) {
       winNum++;
-      console.log(winNum);
       $("#win-num").text(winNum);
       $("#win-lose-message").text("You Win!!!");
       startGame();
     } else if (totalScoreNum > randomNum) {
       loseNum++;
-      console.log(loseNum);
       $("#lose-num").text(loseNum);
       $("#win-lose-message").text("You Lose!!!");
       startGame();
@@ -57,19 +51,16 @@ $(document).ready(function() {
   });
 
   $("#crystal-yellow-img").on("click", function() {
-    console.log((totalScoreNum += crystalYellowNum));
-    console.log(totalScoreNum);
+    totalScoreNum += crystalYellowNum;
     $("#total-score-num").text(totalScoreNum);
 
     if (totalScoreNum === randomNum) {
       winNum++;
-      console.log(winNum);
       $("#win-num").text(winNum);
       $("#win-lose-message").text("You Win!!!");
       startGame();
     } else if (totalScoreNum > randomNum) {
       loseNum++;
-      console.log(loseNum);
       $("#lose-num").text(loseNum);
       $("#win-lose-message").text("You Lose!!!");
       startGame();
@@ -77,19 +68,16 @@ $(document).ready(function() {
   });
 
   $("#crystal-green-img").on("click", function() {
-    console.log((totalScoreNum += crystalGreenNum));
-    console.log(totalScoreNum);
+    totalScoreNum += crystalGreenNum;
     $("#total-score-num").text(totalScoreNum);
 
     if (totalScoreNum === randomNum) {
       winNum++;
-      console.log(winNum);
       $("#win-num").text(winNum);
       $("#win-lose-message").text("You Win!!!");
       startGame();
     } else if (totalScoreNum > randomNum) {
       loseNum++;
-      console.log(loseNum);
       $("#lose-num").text(loseNum);
       $("#win-lose-message").text("You Lose!!!");
       startGame();
@@ -98,12 +86,10 @@ $(document).ready(function() {
 });
 
 function startGame() {
-  console.log("This game has started");
   gameStarted = true;
 
   // At the start of the game, random number is generated
   randomNum = 19 + Math.floor(Math.random() * 120);
-  console.log(randomNum);
   $("#random-num-box").text(randomNum);
 
   //At the start of the game, the crystals are assigned with a random hidden value
@@ -117,13 +103,9 @@ function startGame() {
     crystalYellowNum === crystalGreenNum
   ) {
     crystalRedNum = 1 + Math.floor(Math.random() * 12);
-    console.log("The Red Crystal is: " + crystalRedNum);
     crystalBlueNum = 1 + Math.floor(Math.random() * 12);
-    console.log("The Blue Crystal is: " + crystalBlueNum);
     crystalYellowNum = 1 + Math.floor(Math.random() * 12);
-    console.log("The Yellow Crystal is: " + crystalYellowNum);
     crystalGreenNum = 1 + Math.floor(Math.random() * 12);
-    console.log("The Green Crystal is: " + crystalGreenNum);
   }
 
   if (
@@ -134,14 +116,13 @@ function startGame() {
     crystalBlueNum !== crystalGreenNum &&
     crystalYellowNum !== crystalGreenNum
   ) {
-    console.log("Red Crystal is: " + crystalRedNum);
-    console.log("Blue Crystal is: " + crystalBlueNum);
-    console.log("Yellow Crystal is: " + crystalYellowNum);
-    console.log("Green Crystal is: " + crystalGreenNum);
+    crystalRedNum;
+    crystalBlueNum;
+    crystalYellowNum;
+    crystalGreenNum;
 
     //total score number is set to Zero
     totalScoreNum = 0;
     $("#total-score-num").text(totalScoreNum);
   }
 }
-
